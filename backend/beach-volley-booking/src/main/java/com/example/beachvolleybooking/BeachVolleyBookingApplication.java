f3faa87960eb40c355af506c7731a8a8bd8cc0e2
Mutte18 <mattias.vidhall@hotmail.com>
2023-11-04 14:27:05 +0100

    Added init of spring boot gradle backend

---
+++ backend/beach-volley-booking/src/main/java/com/example/beachvolleybooking/BeachVolleyBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.beachvolleybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeachVolleyBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeachVolleyBookingApplication.class, args);
+	}
+
+}
---
+++ backend/beach-volley-booking/src/test/java/com/example/beachvolleybooking/BeachVolleyBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beachvolleybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeachVolleyBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
