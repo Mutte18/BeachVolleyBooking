8f2cb40923f543e0d7e27cae833e04d19cdae3c8
mattias vidhall <mattias.vidhall@dom.se>
2024-02-12 16:47:38 +0100

    Added initial components and started on login form

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {LoginFormComponent} from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    LoginFormComponent
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
